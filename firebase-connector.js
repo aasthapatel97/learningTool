dfb93c17da7587bffb566792a256e907c817cb3c
Aastha Patel <56579556+aasthapatel97@users.noreply.github.com>
2021-07-31 15:29:52 +0530

    Create firebase-connector.js

---
+++ firebase-connector.js
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.8.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.8.1/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDJ0qgyro6JSHoskRGpQmv8ATbTFh_s9CM",
+    authDomain: "best-learning-tool-949ce.firebaseapp.com",
+    projectId: "best-learning-tool-949ce",
+    storageBucket: "best-learning-tool-949ce.appspot.com",
+    messagingSenderId: "11450420027",
+    appId: "1:11450420027:web:b723c835b0ab4de91e851e",
+    measurementId: "G-TXS189XVFD"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script> 
